60917fbeaac3264c41cb5650f5d7f06c61d7af7e
priyanshu <ginkalpriyanshu@gmail.com>
2024-03-26 15:15:13 +0530

    [Priyanshu] Add: Added RequestModel for register user

---
+++ CommonLayer/RequestModels/RegisterModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CommonLayer.RequestModels
+{
+    public class RegisterModel
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
