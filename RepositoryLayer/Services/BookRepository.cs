c220ace7bab1cf19f47a17033d507c161a95680c
priyanshu <ginkalpriyanshu@gmail.com>
2024-03-27 00:00:47 +0530

    [Priyanshu] Add: Added configuration in BookRepository.cs

---
+++ RepositoryLayer/Services/BookRepository.cs
@@ -0,0 +1,13 @@
+﻿using RepositoryLayer.Context;
+using RepositoryLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.Services
+{
+    public class BookRepository : IBookInterface
+    {
+        private readonly BookStoreContext context;
+    }
+}
