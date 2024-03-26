0151d7ac2635ca5621fe98c2de0dd72feb035573
priyanshu <ginkalpriyanshu@gmail.com>
2024-03-26 15:02:55 +0530

    [Priyanshu] Add: Added Interface for UserRegistration

---
+++ RepositoryLayer/Interface/IUserInterface.cs
@@ -0,0 +1,13 @@
+﻿using CommonLayer.RequestModels;
+using RepositoryLayer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.Interface
+{
+    public interface IUserInterface
+    {
+        public UserEntity UserRegistration(RegisterModel model);
+    }
+}
