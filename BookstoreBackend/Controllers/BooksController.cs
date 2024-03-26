6acf88c3550ad803b3a412a8b6ef78cbe2bc00d4
priyanshu <ginkalpriyanshu@gmail.com>
2024-03-27 01:09:51 +0530

    [Priyanshu] Add: Added BookStoreController and added method to add Book

---
+++ BookstoreBackend/Controllers/BooksController.cs
@@ -0,0 +1,46 @@
+﻿using CommonLayer.RequestModels.BookStore;
+using CommonLayer.ResponseModel;
+using ManagerLayer.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters.Xml;
+using RepositoryLayer.Entity;
+
+namespace BookstoreBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+
+        private readonly IBookManager bookManager;
+        public BooksController(IBookManager bookManager)
+        {
+            this.bookManager = bookManager;
+        }
+        [HttpPost]
+        [Route("AddBook")]
+        public ActionResult AddBooks(AddBookModel model)
+        {
+            var response = bookManager.AddBook(model);
+            if (response != null)
+            {
+                return Ok(new ResponseModel<BooksEntity>
+                {
+                    success = true,
+                    Message = "Book Added Successfully",
+                    Data = response
+                });
+            }
+            else
+            {
+                return BadRequest(new ResponseModel<BooksEntity>
+                {
+                    success = false,
+                    Message = "Failed Adding Book to Database",
+                    Data = response
+                });
+            }
+        }
+    }
+}
