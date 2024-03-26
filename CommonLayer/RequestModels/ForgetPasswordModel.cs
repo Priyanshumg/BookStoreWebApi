f12ebcb2d1e1fb75faa0cc81bcf3eb8683c2bcb9
priyanshu <ginkalpriyanshu@gmail.com>
2024-03-26 21:37:37 +0530

    [Priyanshu] Add: Added new RequestModel, ForgetPassword

---
+++ CommonLayer/RequestModels/ForgetPasswordModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLayer.RequestModels
+{
+    public class ForgetPasswordModel
+    {
+        public string Email { get; set; }
+        public string token { get; set; }
+    }
+}
