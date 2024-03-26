8bff223e80f22747db6f711ac8cf679e4b67ce80
priyanshu <ginkalpriyanshu@gmail.com>
2024-03-26 21:33:10 +0530

    [Priyanshu] Add: Added Login method with GenerateJWTToken method in the UserRepository

--- RepositoryLayer/Services/UserRepository.cs
+++ RepositoryLayer/Services/UserRepository.cs
@@ -1,13 +1,19 @@
 ﻿using CommonLayer.RequestModels;
+using CommonLayer.Utilities;
 using Microsoft.EntityFrameworkCore.Internal;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using RepositoryLayer.Context;
 using RepositoryLayer.Entity;
 using RepositoryLayer.Interface;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading;
@@ -19,9 +25,11 @@ namespace RepositoryLayer.Services
         private readonly BookStoreContext context;
         private static readonly byte[] key = new byte[] { 0x45, 0x6F, 0x3F, 0x12, 0x98, 0xAB, 0xCD, 0xEF, 0x45, 0x6F, 0x3F, 0x12, 0x98, 0xAB, 0xCD, 0xEF };
         private static readonly byte[] iv = new byte[] { 0x45, 0x6F, 0x3F, 0x12, 0x98, 0xAB, 0xCD, 0xEF, 0x45, 0x6F, 0x3F, 0x12, 0x98, 0xAB, 0xCD, 0xEF };
-        public UserRepository(BookStoreContext context) 
+        private IConfiguration _config;
+        public UserRepository(BookStoreContext context, IConfiguration config) 
         {  
-            this.context = context; 
+            this.context = context;
+            this._config = config;
         }
 
         public UserEntity UserRegistration(RegisterModel model)
@@ -44,6 +52,27 @@ namespace RepositoryLayer.Services
             }
         }
 
+        public string UserLogin(LoginModel model)
+        {
+            UserEntity user = context.usersTable.FirstOrDefault(u => u.Email == model.Email);
+            if (user != null)
+            {
+                if (Decrypt(user.Password) == model.Password)
+                {
+                    string token = GenerateToken(user.Email, user.UserId);
+                    return token;
+                }
+                else
+                {
+                    throw new Exception("Invalid Password");
+                }
+            }
+            else
+            {
+                throw new Exception("User Does not exist, please create new account");
+            }
+        }
+
         public static string Encrypt(string UserPassword)
         {
             using Aes aesAlg = Aes.Create();
@@ -63,17 +92,42 @@ namespace RepositoryLayer.Services
 
         public static string Decrypt(string cipherText)
         {
-            using Aes aesAlg = Aes.Create();
-            aesAlg.Key = key;
-            aesAlg.IV = iv;
+            try
+            {
+                using Aes aesAlg = Aes.Create();
+                aesAlg.Key = key;
+                aesAlg.IV = iv;
 
-            ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
 
-            using MemoryStream msDecrypt = new MemoryStream(Convert.FromBase64String(cipherText));
-            using CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read);
-            using StreamReader srDecrypt = new StreamReader(csDecrypt);
-            return srDecrypt.ReadToEnd();
+                using MemoryStream msDecrypt = new MemoryStream(Convert.FromBase64String(cipherText));
+                using CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read);
+                using StreamReader srDecrypt = new StreamReader(csDecrypt);
+                return srDecrypt.ReadToEnd();
+            }
+            catch (Exception ex)
+            {
+                // Log the exception and handle it accordingly
+                Console.WriteLine("Error occurred during decryption: " + ex.Message);
+                throw; // Rethrow the exception to propagate it further if needed
+            }
         }
 
+        private string GenerateToken(string Email, int UserId)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new[]
+            {
+                new Claim("UserEmail",Email),
+                new Claim("UserId", Convert.ToString(UserId))
+            };
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+                _config["Jwt:Audience"],
+                claims,
+                expires: DateTime.Now.AddHours(15),
+                signingCredentials: credentials);
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
     }
 }
