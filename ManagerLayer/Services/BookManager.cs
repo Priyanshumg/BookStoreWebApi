9e0a16ab92d4b85982e1d225a81de955c7716172
priyanshu <ginkalpriyanshu@gmail.com>
2024-03-27 00:02:03 +0530

    [Priyanshu] Add: Added configuration for BookManager in ManagerLayer/Service

---
+++ ManagerLayer/Services/BookManager.cs
@@ -0,0 +1,17 @@
+﻿using ManagerLayer.Interface;
+using RepositoryLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManagerLayer.Services
+{
+    public class BookManager : IBookManager
+    {
+        private readonly IBookInterface repository;
+        public BookManager(IBookInterface repository)
+        {
+            this.repository = repository;
+        }
+    }
+}
