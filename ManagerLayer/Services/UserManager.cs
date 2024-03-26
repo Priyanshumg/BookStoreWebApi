21389d1ba95c56aac87cf53d2d82bdd9b296fea6
priyanshu <ginkalpriyanshu@gmail.com>
2024-03-26 15:05:57 +0530

    [Priyanshu] Add: Added UserRegistration to the ManagerLayer

---
+++ ManagerLayer/Services/UserManager.cs
@@ -0,0 +1,24 @@
+﻿using CommonLayer.RequestModels;
+using ManagerLayer.Interface;
+using RepositoryLayer.Entity;
+using RepositoryLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManagerLayer.Services
+{
+    public class UserManager : IUserManager
+    {
+        private readonly IUserInterface repository;
+        public UserManager(IUserInterface repository)
+        {
+            this.repository = repository;   
+        }
+
+        public UserEntity UserRegistration(RegisterModel model)
+        {
+            return repository.UserRegistration(model);
+        }
+    }
+}
