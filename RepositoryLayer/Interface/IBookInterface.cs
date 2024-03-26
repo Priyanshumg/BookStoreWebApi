5b3dac08c256c2c477985194c6e83e121b157c1f
priyanshu <ginkalpriyanshu@gmail.com>
2024-03-27 01:11:11 +0530

    [Priyanshu] Add: Added BookInterface for BookReposittory

--- RepositoryLayer/Interface/IBookInterface.cs
+++ RepositoryLayer/Interface/IBookInterface.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CommonLayer.RequestModels.BookStore;
+using RepositoryLayer.Entity;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +8,6 @@ namespace RepositoryLayer.Interface
 {
     public interface IBookInterface
     {
+        public BooksEntity AddBook(AddBookModel model);
     }
 }
