8c878af3dccb467f5a125f97b250bcbd7cca2e10
priyanshu <ginkalpriyanshu@gmail.com>
2024-03-26 21:38:03 +0530

    [Priyanshu] Add: Added a method to send token mails

---
+++ CommonLayer/Utilities/SendEmail.cs
@@ -0,0 +1,34 @@
+﻿using CommonLayer.RequestModels;
+using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+
+namespace CommonLayer.Utilities
+{
+    public class SendEmail
+    {
+        public string Sendmail(string ToEmail, string Token)
+        {
+            string FromEmail = "pg6311@srmist.edu.in";
+            MailMessage Message = new MailMessage(FromEmail, ToEmail);
+            string MailBody = "Token Generated : " + Token;
+            Message.Subject = "Here is you reset password OTP";
+            Message.Body = MailBody.ToString();
+            Message.BodyEncoding = Encoding.UTF8;
+            Message.IsBodyHtml = true;
+
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            NetworkCredential credential
+                = new NetworkCredential(FromEmail, "fuks qwjz smlq pezr");
+
+            smtp.EnableSsl = true;
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = credential;
+
+            smtp.Send(Message);
+            return ToEmail;
+        }
+    }
+}
