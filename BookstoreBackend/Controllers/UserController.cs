28599d471fc506df47b181caf42413114cd3ce27
priyanshu <ginkalpriyanshu@gmail.com>
2024-03-26 23:32:25 +0530

    [Priyanshu] Add: Added ResetPassword for WEBAPI controller

--- BookstoreBackend/Controllers/UserController.cs
+++ BookstoreBackend/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using CommonLayer.RequestModels;
 using CommonLayer.ResponseModel;
 using ManagerLayer.Interface;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
@@ -66,6 +67,57 @@ namespace BookstoreBackend.Controllers
                     success = false,
                     Message = "Failed to Login",
                     Data = response
+                });
+            }
+        }
+        [HttpPost]
+        [Route("ForgetPassword")]
+        public ActionResult ForgetPassword(string Email)
+        {
+            var response = userManager.ForgetPassword(Email);
+            if (response != null)
+            {
+                return Ok(new ResponseModel<ForgetPasswordModel>
+                {
+                    success = true,
+                    Message = "Token Sent",
+                    Data = response
+                });
+            }
+            else
+            {
+                return BadRequest(new ResponseModel<ForgetPasswordModel>
+                {
+                    success = false,
+                    Message = "Email Does not exist",
+                    Data = response
+                });
+            }
+        }
+
+        [Authorize]
+        [HttpPost]
+        [Route("ResetPassword")]
+        public IActionResult ResetPassword(ResetPasswordModel reset)
+        {
+            string Email = User.FindFirst("UserEmail").Value;
+            if (userManager.ResetPassword(Email, reset))
+            {
+                return Ok(new ResponseModel<bool>
+                {
+                    success = true,
+                    Message = "Password Reset",
+                    Data = true
+                }
+                );
+            }
+            else
+            {
+                return BadRequest(new ResponseModel<bool>
+                {
+                    success = false,
+                    Message = "Password cannot be reset",
+                    Data = true
                 });
             }
         }
