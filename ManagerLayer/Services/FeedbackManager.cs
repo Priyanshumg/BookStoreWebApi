5df3de6412b2e9a934ad757184b8cbd82f3bd6d6
priyanshu <ginkalpriyanshu@gmail.com>
2024-03-28 01:01:44 +0530

    [Priyanshu] Add: Added AddFeedback to the ManagerLayer

--- ManagerLayer/Services/FeedbackManager.cs
+++ ManagerLayer/Services/FeedbackManager.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using CommonLayer.RequestModels.FeedBackModel;
 using ManagerLayer.Interface;
+using RepositoryLayer.Entity;
+using RepositoryLayer.Interface;
 
 namespace ManagerLayer.Services
 {
     public class FeedbackManager : IFeedbackManager
     {
-
+        private readonly IFeedbackRepository repository;
+        public FeedbackManager(IFeedbackRepository repository)
+        {
+            this.repository = repository;
+        }
+        public FeedBackEntity AddFeedback(AddFeedback model, int UserId)
+        {
+            return repository.AddFeedback(model, UserId);
+        }
+        public List<FeedBackEntity> GetFeedBackByBookId(int Id)
+        {
+            return repository.GetFeedBackByBookId(Id);
+        }
     }
 }
