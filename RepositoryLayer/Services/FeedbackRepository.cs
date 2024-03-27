9e5d79db62e193f4d60835f98215d847eddb2977
priyanshu <ginkalpriyanshu@gmail.com>
2024-03-28 01:00:19 +0530

    [Priyanshu] Add: Added method to AddFeedback in RepositoryLayer/Services

--- RepositoryLayer/Services/FeedbackRepository.cs
+++ RepositoryLayer/Services/FeedbackRepository.cs
@@ -1,12 +1,40 @@
-﻿using RepositoryLayer.Interface;
+﻿using CommonLayer.RequestModels.FeedBackModel;
+using RepositoryLayer.Context;
+using RepositoryLayer.Entity;
+using RepositoryLayer.Interface;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace RepositoryLayer.Services
 {
     public class FeedbackRepository : IFeedbackRepository
     {
+        private readonly BookStoreContext context;
+        public FeedbackRepository(BookStoreContext context)
+        {
+            this.context = context;
+        }
 
+        public FeedBackEntity AddFeedback(AddFeedback model,int UserId)
+        {
+            var book = context.BookTable.FirstOrDefault(book => book.BookId == model.bookId);
+            if (book != null)
+            {
+                FeedBackEntity newFeedback = new FeedBackEntity();
+                newFeedback.BookId = model.bookId;
+                newFeedback.UserId = UserId;
+                newFeedback.CustomerFeedback = model.CustomerFeedback;
+                newFeedback.CustomerRatings = model.CustomerRatings;
+                context.Add(newFeedback);
+                context.SaveChanges();
+                return newFeedback;
+            }
+            else
+            {
+                throw new Exception("Book Not Found");
+            }
+        }
     }
 }
