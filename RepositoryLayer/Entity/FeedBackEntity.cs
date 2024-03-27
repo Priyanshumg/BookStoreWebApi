3fa003b05228ae2c281536ac25dbfbd5921f082f
priyanshu <ginkalpriyanshu@gmail.com>
2024-03-27 23:43:41 +0530

    [Priyanshu] Add: Added Entities for the Feedback

---
+++ RepositoryLayer/Entity/FeedBackEntity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace RepositoryLayer.Entity
+{
+    public class FeedBackEntity
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int FeedBackID { get; set; }
+
+        [ForeignKey("BookTable")]
+        public int BookId { get; set; }
+
+        // Navigation Property for BookID in Feedback
+        public BooksEntity BookTable { get; set; }
+
+        [ForeignKey("UserTable")]
+        public int UserId { get; set; }
+
+        // Navigation Property for BookID in Feedback
+        public UserEntity UserTable { get; set; }
+
+        public float CustomerRatings { get; set; }
+        public string CustomerFeedback { get; set; }
+    }
+}
