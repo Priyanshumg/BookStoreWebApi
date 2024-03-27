90cae71ca15fb83b75e666eba8455842af69ad81
priyanshu <ginkalpriyanshu@gmail.com>
2024-03-28 01:02:20 +0530

    [Priyanshu] Add: Added Interface for ManagerLayer/AddFeedback

--- ManagerLayer/Interface/IFeedbackManager.cs
+++ ManagerLayer/Interface/IFeedbackManager.cs
@@ -1,7 +1,11 @@
-﻿namespace ManagerLayer.Interface
+﻿using CommonLayer.RequestModels.FeedBackModel;
+using RepositoryLayer.Entity;
+using System.Collections.Generic;
+
+namespace ManagerLayer.Interface
 {
     public interface IFeedbackManager
     {
-
+        public FeedBackEntity AddFeedback(AddFeedback model, int UserId);
     }
 }
