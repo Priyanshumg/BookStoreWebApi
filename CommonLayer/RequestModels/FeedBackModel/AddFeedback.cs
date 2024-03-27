171b00b09df1889e2882e276174896b69ae0afdd
priyanshu <ginkalpriyanshu@gmail.com>
2024-03-28 01:05:57 +0530

    [Priyanshu] Add: Added RequestModel to AddFeedback

---
+++ CommonLayer/RequestModels/FeedBackModel/AddFeedback.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace CommonLayer.RequestModels.FeedBackModel
+{
+    public class AddFeedback
+    {
+        public int bookId {  get; set; }
+        public float CustomerRatings { get; set; }
+        public string CustomerFeedback { get; set; }
+    }
+}
