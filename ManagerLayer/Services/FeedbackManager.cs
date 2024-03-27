4c32719262244904b224486e9c464bd205ab5f98
priyanshu <ginkalpriyanshu@gmail.com>
2024-03-27 23:44:04 +0530

    [Priyanshu] Add: Added FeedbackManager in ManagerLayer

---
+++ ManagerLayer/Services/FeedbackManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ManagerLayer.Interface;
+
+namespace ManagerLayer.Services
+{
+    public class FeedbackManager : IFeedbackManager
+    {
+
+    }
+}
