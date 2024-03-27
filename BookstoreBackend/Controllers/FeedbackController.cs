db484ff49986d01b5cc9fb6be5acddf256223d8a
priyanshu <ginkalpriyanshu@gmail.com>
2024-03-28 01:03:10 +0530

    [Priyanshu] Add: Added AddFeedback to the FeedbackController inside same method

--- BookstoreBackend/Controllers/FeedbackController.cs
+++ BookstoreBackend/Controllers/FeedbackController.cs
@@ -1,5 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CommonLayer.RequestModels.FeedBackModel;
+using CommonLayer.ResponseModel;
+using ManagerLayer.Interface;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RepositoryLayer.Entity;
+using System;
+using System.Collections.Generic;
 
 namespace BookstoreBackend.Controllers
 {
@@ -7,6 +14,37 @@ namespace BookstoreBackend.Controllers
     [ApiController]
     public class FeedbackController : ControllerBase
     {
+        private readonly IFeedbackManager feedbackManager;
+        public FeedbackController(IFeedbackManager feedbackManager)
+        {
+            this.feedbackManager = feedbackManager;
+        }
 
+        [HttpPost]
+        [Authorize]
+        [Route("AddFeedback")]
+        public ActionResult AddFeedback(AddFeedback model)
+        {
+            int UserId = Convert.ToInt32(User.FindFirst("UserId").Value);
+            var response = feedbackManager.AddFeedback(model, UserId);
+            if (response != null)
+            {
+                return Ok(new ResponseModel<FeedBackEntity>
+                {
+                    success = true,
+                    Message = "Feedback Added Successfully",
+                    Data = response
+                });
+            }
+            else
+            {
+                return BadRequest(new ResponseModel<FeedBackEntity>
+                {
+                    success = false,
+                    Message = "Could not add feedback",
+                    Data = response
+                });
+            }
+        }
     }
 }
