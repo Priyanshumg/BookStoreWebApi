94cf052a3c070d5e38746ff901435450e9f80c3f
Priyanshumg <ginkalpriyanshu@gmail.com>
2024-03-27 15:37:13 +0530

    [Priyanshu] Add: Added Migration for the database, ColName - CreatedAt, UpdatedAt

---
+++ RepositoryLayer/Migrations/20240327100139_BookTableUpdate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RepositoryLayer.Migrations
+{
+    public partial class BookTableUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "BookTable",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "BookTable",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "BookTable");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "BookTable");
+        }
+    }
+}
