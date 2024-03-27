66b5caf0b48cb365fdb9fc42a9174da4439d930f
priyanshu <ginkalpriyanshu@gmail.com>
2024-03-27 23:41:20 +0530

    [Priyanshu] Add: Added new table for the Feedback

--- RepositoryLayer/Context/BookStoreContext.cs
+++ RepositoryLayer/Context/BookStoreContext.cs
@@ -15,5 +15,7 @@ namespace RepositoryLayer.Context
 
         public DbSet<UserEntity> usersTable { get; set; }
         public DbSet<BooksEntity> BookTable { get; set; }
+        public DbSet<FeedBackEntity> FeedbackTable { get; set; }
+
     }
 }
