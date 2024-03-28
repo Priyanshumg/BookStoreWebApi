14c769d68ca531f88a65094117607d2df732dc98
priyanshu <ginkalpriyanshu@gmail.com>
2024-03-28 15:47:01 +0530

    [Priyanshu] Add: Added MyCartController to the controller

--- BookstoreBackend/Controllers/MyCartController.cs
+++ BookstoreBackend/Controllers/MyCartController.cs
@@ -1,5 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CommonLayer.RequestModels.MyCart;
+using CommonLayer.ResponseModel;
+using ManagerLayer.Interface;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RepositoryLayer.Entity;
+using System;
 
 namespace BookstoreBackend.Controllers
 {
@@ -8,5 +14,37 @@ namespace BookstoreBackend.Controllers
     public class MyCartController : ControllerBase
     {
 
+        private readonly IMyCartManager myCartManager;
+        public MyCartController(IMyCartManager myCartManager)
+        {
+            this.myCartManager = myCartManager;
+        }
+
+        [Authorize]
+        [HttpPost]
+        [Route("AddToCart")]
+        public ActionResult AddToCart(AddToCartModel model)
+        {
+            int UserId = Convert.ToInt32(User.FindFirst("UserId").Value);
+            var response = myCartManager.AddToCart(model, UserId);
+            if (response != null)
+            {
+                return Ok(new ResponseModel<MyCartEntity>
+                {
+                    success = true,
+                    Message = "Book Added To cart Successfully",
+                    Data = response
+                });
+            }
+            else
+            {
+                return BadRequest(new ResponseModel<MyCartEntity>
+                {
+                    success = false,
+                    Message = "Failed To Add into Cart",
+                    Data = response
+                });
+            }
+        }
     }
 }
