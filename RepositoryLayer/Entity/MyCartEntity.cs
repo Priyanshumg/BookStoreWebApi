b058f89986fb60fede38ac5baeea625b7342e133
priyanshu <ginkalpriyanshu@gmail.com>
2024-03-28 15:51:56 +0530

    [Priyanshu] Add: Added MyCartEntity for the database context

--- RepositoryLayer/Entity/MyCartEntity.cs
+++ RepositoryLayer/Entity/MyCartEntity.cs
@@ -9,18 +9,19 @@ namespace RepositoryLayer.Entity
     public class MyCartEntity
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int CartId { get; set; }
 
         [ForeignKey("BookTable")]
         public int BookId { get; set; }
 
-        // Navigation Property for BookTable
+        // Navigation Property for BookID in Feedback
         public BooksEntity BookTable { get; set; }
 
         [ForeignKey("UserTable")]
         public int UserId { get; set; }
 
-        // Navigation Property for UserTable
+        // Navigation Property for BookID in Feedback
         public UserEntity UserTable { get; set; }
 
         public int Quantity { get; set; }
