10d483be3e6ebdedebd020f1656bac7f2ba2226e
priyanshu <ginkalpriyanshu@gmail.com>
2024-03-28 12:09:25 +0530

    [Priyanshu] Add: Added miigration to database for MyCartTable

--- RepositoryLayer/Migrations/20240328062728_CartTable.Designer.cs
+++ RepositoryLayer/Migrations/20240328062728_CartTable.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RepositoryLayer.Context;
 
 namespace RepositoryLayer.Migrations
 {
     [DbContext(typeof(BookStoreContext))]
-    partial class BookStoreContextModelSnapshot : ModelSnapshot
+    [Migration("20240328062728_CartTable")]
+    partial class CartTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ RepositoryLayer/Migrations/20240328062728_CartTable.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RepositoryLayer.Migrations
+{
+    public partial class CartTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MyCartTable",
+                columns: table => new
+                {
+                    CartId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BookId = table.Column<int>(nullable: false),
+                    UserId = table.Column<int>(nullable: false),
+                    Quantity = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MyCartTable", x => x.CartId);
+                    table.ForeignKey(
+                        name: "FK_MyCartTable_BookTable_BookId",
+                        column: x => x.BookId,
+                        principalTable: "BookTable",
+                        principalColumn: "BookId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MyCartTable_usersTable_UserId",
+                        column: x => x.UserId,
+                        principalTable: "usersTable",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MyCartTable_BookId",
+                table: "MyCartTable",
+                column: "BookId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MyCartTable_UserId",
+                table: "MyCartTable",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MyCartTable");
+        }
+    }
+}
