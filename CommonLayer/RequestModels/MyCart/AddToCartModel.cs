20daa68f503a2ccf1b56689b008e781d94d94991
priyanshu <ginkalpriyanshu@gmail.com>
2024-03-28 15:46:22 +0530

    [Priyanshu] Add: Added MyCartModel in RequestModel

---
+++ CommonLayer/RequestModels/MyCart/AddToCartModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace CommonLayer.RequestModels.MyCart
+{
+    public class AddToCartModel
+    {
+        public int BookId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
