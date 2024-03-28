2a610e1db81a0611c52df91b38d67dfcb87788be
priyanshu <ginkalpriyanshu@gmail.com>
2024-03-28 12:08:49 +0530

    [Priyanshu] Add: Added MyCartEntity in repositoryLayer

---
+++ RepositoryLayer/Entity/MyCartEntity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace RepositoryLayer.Entity
+{
+    public class MyCartEntity
+    {
+        [Key]
+        public int CartId { get; set; }
+
+        [ForeignKey("BookTable")]
+        public int BookId { get; set; }
+
+        // Navigation Property for BookTable
+        public BooksEntity BookTable { get; set; }
+
+        [ForeignKey("UserTable")]
+        public int UserId { get; set; }
+
+        // Navigation Property for UserTable
+        public UserEntity UserTable { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
