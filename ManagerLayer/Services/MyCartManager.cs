0bb56ff57712de3138a78b76991358060459b95a
priyanshu <ginkalpriyanshu@gmail.com>
2024-03-28 12:07:45 +0530

    [Priyanshu] Add: Added ManagerLayerService element MycartManager

---
+++ ManagerLayer/Services/MyCartManager.cs
@@ -0,0 +1,17 @@
+﻿using ManagerLayer.Interface;
+using RepositoryLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManagerLayer.Services
+{
+    public class MyCartManager : IMyCartManager
+    {
+        private readonly IMyCartInterface repository;
+        public MyCartManager(IMyCartInterface repository)
+        {
+            this.repository = repository;
+        }
+    }
+}
