91550b279e49aa4858e98a5f5aaaf4ec38d9104f
priyanshu <ginkalpriyanshu@gmail.com>
2024-03-29 20:41:11 +0530

    [Priyanshu] Add: Added ManagerLayer configuration for WishlistMethods

---
+++ ManagerLayer/Services/WishListManager.cs
@@ -0,0 +1,18 @@
+﻿using ManagerLayer.Interface;
+using RepositoryLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace ManagerLayer.Services
+{
+    public class WishListManager : IWishListManager
+    {
+        private readonly IWishListRepository repository;
+        public WishListManager(IWishListRepository repository)
+        {
+            this.repository = repository;
+        }
+    }
+}
