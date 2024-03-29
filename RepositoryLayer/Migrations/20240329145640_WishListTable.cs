beb826ce84dc5a7ed271337f0ab718856d9e05be
priyanshu <ginkalpriyanshu@gmail.com>
2024-03-29 20:38:48 +0530

    [Priyanshu] Add: Added new migration for wishlistTable and updated database

---
+++ RepositoryLayer/Migrations/20240329145640_WishListTable.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RepositoryLayer.Migrations
+{
+    public partial class WishListTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WishListTable",
+                columns: table => new
+                {
+                    WishListId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(nullable: false),
+                    BookId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WishListTable", x => x.WishListId);
+                    table.ForeignKey(
+                        name: "FK_WishListTable_BookTable_BookId",
+                        column: x => x.BookId,
+                        principalTable: "BookTable",
+                        principalColumn: "BookId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_WishListTable_usersTable_UserId",
+                        column: x => x.UserId,
+                        principalTable: "usersTable",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WishListTable_BookId",
+                table: "WishListTable",
+                column: "BookId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WishListTable_UserId",
+                table: "WishListTable",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WishListTable");
+        }
+    }
+}
