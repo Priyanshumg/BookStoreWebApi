2cfbc7c6bfd41d9d7e76525d1099d5e60dd18b30
priyanshu <ginkalpriyanshu@gmail.com>
2024-03-29 20:39:27 +0530

    [Priyanshu] Add: Added Configuration for the WishList Repository

---
+++ RepositoryLayer/Services/WishListRepository.cs
@@ -0,0 +1,17 @@
+﻿using RepositoryLayer.Context;
+using RepositoryLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.Services
+{
+    public class WishListRepository : IWishListRepository
+    {
+        private readonly BookStoreContext context;
+        public WishListRepository(BookStoreContext context)
+        {
+            this.context = context;
+        }
+    }
+}
