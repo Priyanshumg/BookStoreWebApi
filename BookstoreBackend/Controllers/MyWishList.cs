83a5512789e9248f3e7b04bbd133b1aefa58df47
priyanshu <ginkalpriyanshu@gmail.com>
2024-04-01 12:07:14 +0530

    [Priyanshu] Add: Added Controller for MyWishList.cs

--- BookstoreBackend/Controllers/MyWishList.cs
+++ BookstoreBackend/Controllers/MyWishList.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using RepositoryLayer.Entity;
 using System;
+using System.Collections.Generic;
 
 namespace BookstoreBackend.Controllers
 {
@@ -38,6 +39,21 @@ namespace BookstoreBackend.Controllers
                 return BadRequest(new ResponseModel<MyWishListEntity> { success = false, Message = ex.Message });
             }
         }
+
+        [Authorize]
+        [HttpGet]
+        [Route("GetAllWishListBooks")]
+        public ActionResult GetAllWishListBook()
+        {
+            int userId = Convert.ToInt32(User.FindFirst("UserId").Value);
+            var response = manager.GetAllWishListNotes(userId);
+            if (response != null)
+            {
+                return Ok(new ResponseModel<List<MyWishListEntity>> { success = true, Message = "Fetched All notes successfully", Data = response });
+            }
+            return BadRequest(new ResponseModel<List<MyWishListEntity>> { success = false, Message = "Fetching of notes failed", Data = null });
+        }
+
 
     }
 }
