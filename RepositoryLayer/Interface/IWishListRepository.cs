78627b4778e483827dabf1b35475f16ac4feda8c
priyanshu <ginkalpriyanshu@gmail.com>
2024-04-01 11:38:11 +0530

    [Priyanshu] Add: Added interface for the add to WishList method in repository layer

--- RepositoryLayer/Interface/IWishListRepository.cs
+++ RepositoryLayer/Interface/IWishListRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RepositoryLayer.Entity;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +7,6 @@ namespace RepositoryLayer.Interface
 {
     public interface IWishListRepository
     {
-
+        public MyWishListEntity AddToWishList(int UserId, int BookId);
     }
 }
