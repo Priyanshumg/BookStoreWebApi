273478fb5e885e7698af43e90bfec83bcfa95508
priyanshu <ginkalpriyanshu@gmail.com>
2024-04-01 11:38:44 +0530

    [Priyanshu] Add: Added Add WishList to repository Layer

--- RepositoryLayer/Services/WishListRepository.cs
+++ RepositoryLayer/Services/WishListRepository.cs
@@ -1,7 +1,9 @@
 ﻿using RepositoryLayer.Context;
+using RepositoryLayer.Entity;
 using RepositoryLayer.Interface;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace RepositoryLayer.Services
@@ -12,6 +14,21 @@ namespace RepositoryLayer.Services
         public WishListRepository(BookStoreContext context)
         {
             this.context = context;
+        }
+
+        public MyWishListEntity AddToWishList(int UserId, int BookId)
+        {
+            var existingbook = context.WishListTable.FirstOrDefault(x => x.UserId == UserId && x.BookId == BookId);
+            if (existingbook == null)
+            {
+                MyWishListEntity newbook = new MyWishListEntity();
+                newbook.UserId = UserId;
+                newbook.BookId = BookId;
+                context.WishListTable.Add(newbook);
+                context.SaveChanges();
+                return newbook;
+            }
+            throw new Exception("Book Already exist in the wishList");
         }
     }
 }
